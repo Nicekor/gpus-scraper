5f5eb47a20d8aadcccad9fe6d6145ee141e4793c
Jose <zemorais16@hotmail.com>
2021-03-09 02:13:26 +0000

    creating pool instead of connection

--- db/index.js
+++ db/index.js
@@ -2,11 +2,12 @@ import mysql from 'mysql2/promise';
 import dotenv from 'dotenv';
 
 dotenv.config();
-const sqlPromise = mysql.createConnection({
+const sqlPromise = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
+  connectionLimit: 10,
 });
 
 (async () => {
